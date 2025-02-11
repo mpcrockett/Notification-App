4ee614cf6e4612265d2598585ea92871a1fd38b3
mpcrockett <mpcrockett+github@gmail.com>
2025-02-10 20:59:28 -0500

    chakra provider and room buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button, Grid, GridItem } from "@chakra-ui/react"
+import { colorPalettes } from "compositions/lib/color-palettes"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [room, setRoom] = useState<number | null>(0);
+  // const [therapist, setTherapist] = useState<number | null>(0);
+
+  const rooms: { [key: number]: string } = {
+    1: "Room 1",
+    2: "Room 2",
+    3: "Room 3",
+    4: "Room 4",
+    5: "Room 5",
+    6: "Room 6",
+    7: "Room 7",
+    8: "Room 8",
+    9: "Room 9",
+    10: "Room 10",
+    11: "Room 11",
+    12: "Room 12",
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Grid gap="6" direction="row">
+      {Object.keys(rooms).map((key) => (
+        <Button key={key} onClick={() => setRoom(parseInt(key))} size="xl">
+          {rooms[parseInt(key)]}
+        </Button>
+      ))}
+    </Grid>
   )
-}
+};
 
 export default App
